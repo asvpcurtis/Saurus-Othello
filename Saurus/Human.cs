1f8a07d5040b7a6e1c00d9ca0602db5918100778
plzb0ss <curtisw0234@gmail.com>
2016-08-20 22:55:25 -0400

    adding player classes without implementation

---
+++ Saurus/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saurus
+{
+    class Human : IPlayer
+    {
+        public void moveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Report(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Saurus/Saurus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saurus
+{
+    class Saurus : IPlayer
+    {
+        public void moveAsync()
+        {
+            //Task thinking;
+            //thinking = Task.Run(() => move(ref mainBoard, moveList));
+            throw new NotImplementedException();
+        }
+
+        public void Report(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
