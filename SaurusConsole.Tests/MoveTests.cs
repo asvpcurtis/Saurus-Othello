ef220c14cdef4e4844b041795863bab7d76a1fef
plzb0ss <curtisw0234@gmail.com>
2017-12-03 00:27:09 -0500

    Test if a failed test will fail the build

--- SaurusConsole.Tests/MoveTests.cs
+++ SaurusConsole.Tests/MoveTests.cs
@@ -22,6 +22,7 @@ namespace SaurusConsoleTests
 
             Move h8move = new Move("H8");
             Assert.AreEqual("H8", h8move.ToString());
+            Assert.Fail();
         }
 
         [TestMethod]
